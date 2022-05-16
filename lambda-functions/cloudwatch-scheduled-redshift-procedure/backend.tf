0672563fd865260ab99d36c9572d1613c536e36f
Quentin Gaborit <gibboneto@gmail.com>
2022-05-16 16:48:13 +0200

    chore: Change backends tfstate file key

--- lambda-functions/cloudwatch-scheduled-redshift-procedure/backend.tf
+++ lambda-functions/cloudwatch-scheduled-redshift-procedure/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket                  = "gbto-tfstates-sbx"
-    key                     = "gbto-lambda-cloudwatch-redshift-procedure-dev.tfstate"
+    key                     = "gbto-lambda-cloudwatch-redshift-dev.tfstate"
     region                  = "us-east-1"
     shared_credentials_file = "~/.aws/credentials"
     profile                 = "default"
--- lambda-functions/s3-event-notifications-python-lambda/backend.tf
+++ lambda-functions/s3-event-notifications-python-lambda/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket                  = "gbto-tfstates-sbx"
-    key                     = "gbto-lambda-s3-events-python-dev.tfstate"
+    key                     = "gbto-lambda-s3-notifications-dev.tfstate"
     region                  = "us-east-1"
     shared_credentials_file = "~/.aws/credentials"
     profile                 = "default"
